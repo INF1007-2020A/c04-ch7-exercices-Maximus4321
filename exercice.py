ce05a5f14b06ca1b3c85d0528565ae3bf72efe4c
Maximus4321 <70984715+Maximus4321@users.noreply.github.com>
2020-10-06 19:32:37 -0400

    Update exercice.py

--- exercice.py
+++ exercice.py
@@ -3,9 +3,20 @@
 
 # TODO: Importez vos modules ici
 
+import math
+import sys
+sys.path.insert(1,C:\Users\Maxime\Documents\GitHub\c04-ch6-exercices-Maximus4321)
+from exercice2 import frequence
 
-
+import turtle
 # TODO: Définissez vos fonction ici
+
+def compute_volume_mass(a=2,b=4,c=2,p=10):
+    volume=(4/3)*math.pi**a*b*c
+    mass=p*volume
+    return mass,volume
+
+
 
 
 if __name__ == '__main__':
